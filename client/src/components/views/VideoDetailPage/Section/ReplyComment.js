7d8fd26dc07654ca70f5bc4e791c9dd40cac1757
jiseong-choi <jiseong1951@kakao.com>
2020-12-17 03:43:28 +0900

    I am a real developer

--- client/src/components/views/VideoDetailPage/Section/ReplyComment.js
+++ client/src/components/views/VideoDetailPage/Section/ReplyComment.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import SingleComment from './SingleComment'
 
-function ReplyComment() {
+function ReplyComment(props) {
+
+    const renderReplyComment = (parentCommentId) => {
+        props.CommentLists.map((comment, index) => (
+            <React.Fragment>
+                {
+                    comment.responseTo === parentCommentId &&
+                    <div>
+                        <SingleComment refreshFunction={props.refreshFunction} key={index} comment={comment} postId={props.postId} />
+                        <ReplyComment  CommentLists={props.CommentLists} />
+                    </div>
+                }
+            </React.Fragment>
+        ))
+    }
+
     return (
         <div>
-            ReplyComment
+            <p style={{fontSize:'14px',margin:'0',clolor:'gray'}} onClick >
+                View 1 more comment(s)
+            </p>
+            {renderReplyComment(props.parentCommentId)}
         </div>
     )
 }
